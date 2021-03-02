4bee100f23b93363488a246dcc1dd87dd04ef853
Gaurav Singh <2363826+0xf17@users.noreply.github.com>
2021-03-02 22:52:49 +0530

    Add files via upload

---
+++ 2020/behind-the-scene.php
@@ -0,0 +1,79 @@
+<?php
+include '../config.php';
+?>
+
+<?php include '../partials/pre-load.php'; ?>
+</style>
+<body>
+    <div class="container-fluid">
+
+        <!-- HEADER -->
+        <div class="row align-items-start">
+            <div class="col-lg-12">
+                <h1 class="sans"><a href="/">Deconstructing Algorithms</a> / 2020
+                    <?php include '../partials/dark-theme-toggle.php'; ?></h1>
+            </div>
+        </div>
+
+        <!-- CONTENT-TOP -->
+        <div class="row align-items start mt-3">
+            <div class="col-lg-8">
+                <p class="sans big">Behind the scene</p>
+            </div>
+        </div>
+
+
+        <!-- CONTENT-MAIN -->
+        <div class="row align-items start mt-3">
+            <div class="col-lg-6">
+                <div class="gallery-item mb-3">
+                    <img src="oribaka-shoot.jpg">
+                </div>
+                <p class="sans mt-2">Invitation</p>
+            </div>
+            <div class="col-lg-6">
+                <div class="gallery-item mb-3">
+                    <img src="visit-to-science-gallery-blr-2020.jpg">
+                </div>
+                <p class="sans mt-2">Invitation</p>
+            </div>
+            <div class="col-lg-6">
+                <div class="gallery-item mb-3">
+                    <img src="oribaka-language.jpg">
+                </div>
+                <p class="sans mt-2">Invitation</p>
+            </div>
+            <div class="col-lg-6">
+                <div class="gallery-item mb-3">
+                    <img src="oribaka-exhibition.jpg">
+                </div>
+                <p class="sans mt-2">Invitation</p>
+            </div>
+            <div class="col-lg-6">
+                <div class="gallery-item mb-3">
+                    <img src="407.jpg">
+                </div>
+                <p class="sans mt-2">Invitation</p>
+            </div>
+            <div class="col-lg-6">
+                <div class="gallery-item mb-3">
+                    <img src="deciphering.jpg">
+                </div>
+                <p class="sans mt-2">Invitation</p>
+            </div>
+            <div class="col-lg-6">
+                <div class="video-container mb-3">
+                    <iframe src="https://player.vimeo.com/video/506663841" width="100%" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="video-container mb-3">
+                    <iframe src="https://player.vimeo.com/video/506738352" width="100%" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include '../partials/post-load.php'; ?>
+</body>
+
+</html>
