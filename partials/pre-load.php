3f804681af90256ee7c0a34664863fc1c24f8f62
0xf17 <grv@mathscapes.xyz>
2021-03-04 08:57:19 +0530

    updated aux urls

--- partials/pre-load.php
+++ partials/pre-load.php
@@ -9,7 +9,7 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <link href="http://mathscapes.xyz/aux/css/light.css" rel="stylesheet" id="theme-link">
-    <link href="http://mathscapes.xyz/aux/css/style.css" rel="stylesheet">
+    <link href="https://mathscapes.xyz/aux/css/light.css" rel="stylesheet" id="theme-link">
+    <link href="https://mathscapes.xyz/aux/css/style.css" rel="stylesheet">
     <title>Deconstructing Algorithms</title>
   </head>
